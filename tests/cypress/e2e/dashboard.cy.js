841bfbf7d06d9d3963908453ede273cfa58c15b5
Admiralty02 <jakochieng01@gmail.com>
2025-07-10 17:37:27 +0300

    Update Cypress test URLs to localhost

--- tests/cypress/e2e/dashboard.cy.js
+++ tests/cypress/e2e/dashboard.cy.js
@@ -1,11 +1,11 @@
 describe('Dashboard & Analytics', () => {
   it('Displays dashboard stats', () => {
-    cy.visit('http://localhost:3000/dashboard');
+    cy.visit('https://686b95c4f8c3190008f5a8f0--cleancity-waste-management.netlify.app/dashboard');
     cy.contains('Statistics').should('exist');
   });
 
   it('Shows leaderboards and charts', () => {
-    cy.visit('http://localhost:3000/dashboard');
+    cy.visit('https://686b95c4f8c3190008f5a8f0--cleancity-waste-management.netlify.app/dashboard');
     cy.contains('Leaderboard').should('exist');
     cy.get('canvas').should('exist');
   });
